b2435e77e4fcf75f7a048a3274691fc26aa440f1
Haeleon <matthewj52402@gmail.com>
2024-01-06 22:53:46 -0600

    Add reinstaller

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Epoch
 A system of programs and libraries intended for CC: Tweaked
+
+To install, simply run:
+`wget run https://raw.githubusercontent.com/Haeleon/Epoch/main/installer.lua`
--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
 shell.run("delete epoch/")
-shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/installer.lua epoch/installer.lua")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/reinstall.lua epoch/reinstall.lua")
 shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/self.lua epoch/self.lua")
 shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/fast_excavate.lua epoch/fast_excavate.lua")
