6ed776e283872ad8d7d67e9a103f85865e63dab0
Haeleon <matthewj52402@gmail.com>
2024-01-06 22:35:49 -0600

    Add basic installer

---
+++ installer.lua
@@ -0,0 +1,3 @@
+shell.run("delete epoch/")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/self.lua epoch/self.lua")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/fast_excavate.lua epoch/fast_excavate.lua")
