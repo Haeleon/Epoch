bb391f821d6a4bfb194347b4d220f7fe198f6afa
Haeleon <matthewj52402@gmail.com>
2024-01-20 00:48:49 -0600

    Add new programs to the installer

--- programs/installer.lua
+++ programs/installer.lua
@@ -1,4 +1,10 @@
 shell.run("delete epoch/")
-shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/reinstall.lua epoch/reinstall.lua")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/install.lua epoch/install.lua")
+
+-- Libraries/deps
 shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/self.lua epoch/self.lua")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/self.lua epoch/build.lua")
+
+-- Programs/commands
 shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/fast_excavate.lua epoch/fast_excavate.lua")
+shell.run("wget https://raw.githubusercontent.com/Haeleon/Epoch/main/programs/fill.lua epoch/fill.lua")
