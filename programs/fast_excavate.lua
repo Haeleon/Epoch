339ccc1bc7175812372a1fc67614394dac18ae6a
Haeleon <30844056+Haeleon@users.noreply.github.com>
2024-07-07 13:24:53 -0500

    Fix fast_excavate edge case

--- programs/fast_excavate.lua
+++ programs/fast_excavate.lua
@@ -1,8 +1,8 @@
--- fast_excavate v1
+-- fast_excavate v1.0.3
 
 if not turtle then
-	print("This program requires a turtle")
-	exit()
+    print("This program requires a turtle")
+    exit()
 end
 
 local tArgs = { ... }
@@ -20,76 +20,76 @@ local maxDepth = tonumber(tArgs[3]) or 1000000
 local facing = false;
 
 local function unload()
-	local bookmark = self.getPosition();
-	self.goToPosition(0, 0, 0, 2);
-	for i=1,16 do
-		self.select(i);
-		self.drop(64);
-	end
-	self.select(1);
-	self.goToPosition(bookmark);
+    local bookmark = self.getPosition();
+    self.goToPosition(0, 0, 0, 2);
+    for i=1,16 do
+        self.select(i);
+        self.drop(64);
+    end
+    self.select(1);
+    self.goToPosition(bookmark);
 end
 
 local function endProgram()
-	self.goToPosition(0, 0, 0, 0);
-	print("Finished!");
+    self.goToPosition(0, 0, 0, 0);
+    print("Finished!");
 end
 
 local function refuel()
-	self.refuel()
+    self.refuel()
 end
 
 local function miningCycle()
-		if not self.forward() then goto done end
-		if self.getItemCount(15) > 0 then 
-			if self.back() then unload() self.forward() else unload() end
-		end
-		self.digUp();
-		if self.getItemCount(15) > 0 then unload() end
-		self.digDown();
-		if self.getItemCount(15) > 0 then unload() end
+        if not self.forward() then return false end
+        if self.getItemCount(15) > 0 then 
+            if self.back() then unload() self.forward() else unload() end
+        end
+        self.digUp();
+        if self.getItemCount(15) > 0 then unload() end
+        self.digDown();
+        if self.getItemCount(15) > 0 then unload() end
+        return true
 end
 
 local function main()
-	self.forced = true;
-	local cont = false;
-	
-	local p = self.getPosition();
-	p.y = p.y-1; -- Start only one tile below the chest
-	-- Repeat while it can move down
-	while self.goToPosition(p) do
-		-- Make sure to grab the block dircetly under it when it moves down
-		self.digDown()
-		if self.getItemCount(15) > 0 then unload() end
-		for width=1,x do
-			if cont then 
-				if facing then
-					self.turnLeft()
-						miningCycle()
-					self.turnLeft()
-				else
-					self.turnRight()
-						miningCycle()
-					self.turnRight()
-				end
-				facing = not facing
-			end
-			cont = true;
-			for depth=2,y do
-				miningCycle()
-			end
-		end
---		self.turnLeft()
---		self.turnLeft()
-		p = self.getPosition();
-		p.y = p.y-3;
-		p.h = (p.h+2)%4;
---		if x%2 == 1 then facing = not facing end
---		facing = not facing
-		cont = false;
-	end
-	::done::
-	endProgram()
+    self.forced = true;
+    local cont = false;
+    
+    local p = self.getPosition();
+    p.y = p.y-1; -- Start only one tile below the chest
+    -- Repeat while it can move down
+    while self.goToPosition(p) do
+        -- Make sure to grab the block dircetly under it when it moves down
+        self.digDown()
+        if self.getItemCount(15) > 0 then unload() end
+        for width=1,x do
+            if cont then 
+                if facing then
+                    self.turnLeft()
+                        if not miningCycle() then return 0 end
+                    self.turnLeft()
+                else
+                    self.turnRight()
+                        if not miningCycle() then return 0 end
+                    self.turnRight()
+                end
+                facing = not facing
+            end
+            cont = true;
+            for depth=2,y do
+                if not miningCycle() then return 0 end
+            end
+        end
+--        self.turnLeft()
+--        self.turnLeft()
+        p = self.getPosition();
+        p.y = p.y-3;
+        p.h = (p.h+2)%4;
+--        if x%2 == 1 then facing = not facing end
+--        facing = not facing
+        cont = false;
+    end
 end
 
 main()
+endProgram()
