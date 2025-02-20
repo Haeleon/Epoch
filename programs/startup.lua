dc8e0334fabca5e6b148fa79acaf823a82debf3c
Haeleon <matthewj52402@gmail.com>
2025-02-20 00:37:59 -0600

    fix: Fix bug in resetAliases.lua

--- programs/startup.lua
+++ programs/startup.lua
@@ -1,5 +1,7 @@
 -- This file was created by the Epoch installer
 
+shell.run('/epoch/resetAliases.lua')
+
 if _G.isEpochHost then 
     if _G.epochHostShell then
         print("Host is already running")
@@ -11,5 +13,4 @@ if _G.isEpochHost then
 end
 
 shell.run('/epoch/update.lua')
-shell.run('/epoch/resetAliases.lua')
 shell.run('/epoch/toolbar.lua')
