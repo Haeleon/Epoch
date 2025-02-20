a455c3ae7c1d551a1c72d94f45ee3913d4858fc1
Haeleon <matthewj52402@gmail.com>
2025-02-20 00:22:27 -0600

    feat: Make installer.lua generate resetAliases.lua, which was added to startup.lua

--- programs/startup.lua
+++ programs/startup.lua
@@ -1,3 +1,5 @@
+-- This file was created by the Epoch installer
+
 if _G.isEpochHost then 
     if _G.epochHostShell then
         print("Host is already running")
@@ -9,4 +11,5 @@ if _G.isEpochHost then
 end
 
 shell.run('/epoch/update.lua')
+shell.run('/epoch/resetAliases.lua')
 shell.run('/epoch/toolbar.lua')
